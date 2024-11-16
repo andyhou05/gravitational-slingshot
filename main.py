e7c716895dab05f2c25f652cc777cc5177792981
Andy <andy.hou1604@gmail.com>
2024-11-16 07:51:56 -0500

    added projectile list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from models.projectile import Projectile
 
 # pygame setup
 pygame.init()
@@ -7,8 +8,8 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 running = True
 
-# Store the circles as (position, size)
-circles = []
+# Store the projectile objects
+projectiles = []
 
 # Colors:
 RED = (255, 0, 0)  # For planets
@@ -23,20 +24,16 @@ while running:
         # Detect mouse click
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left mouse button
-                # Get mouse position
-                mouse_pos = pygame.mouse.get_pos()
-                # Random size for the circle
-                size = random.randint(10, 20)
-                # Add circle data to the list
-                circles.append((mouse_pos, size))
-                print(circles)
+                # Create new projectile
+                projectile = Projectile(0, position = pygame.mouse.get_pos(), radius = random.randint(10, 20))
+                projectiles.append(projectile)
 
     # Fill the screen with a color to wipe away anything from the last frame
     screen.fill("black")
 
     # Draw all circles stored in the list
-    for pos, size in circles:
-        pygame.draw.circle(screen, BLUE, pos, size)
+    for projectile in projectiles:
+        pygame.draw.circle(screen, BLUE, projectile.position, projectile.radius)
 
     # Flip the display to put your work on the screen
     pygame.display.flip()
@@ -45,4 +42,3 @@ while running:
     clock.tick(60)
 
 pygame.quit()
-# TEST
--- models/projectile.py
+++ models/projectile.py
@@ -1,6 +1,5 @@
 class Projectile:
-    def __init__(self, velocity, x_position, y_position, radius):
+    def __init__(self, velocity, position, radius):
         self.velocity = velocity
-        self.x_position = x_position
-        self.y_position = y_position
+        self.position = position
         self.radius = radius
