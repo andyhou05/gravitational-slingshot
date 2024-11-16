3609eb65651ac45b631c57089cbba35d3b0cc355
Andy Hou <andy.hou1604@gmail.com>
2024-11-15 20:41:54 -0500

    created projectile class

---
+++ models/projectile.py
@@ -0,0 +1,6 @@
+class Projectile:
+    def __init__(self, velocity, x_position, y_position, radius):
+        self.velocity = velocity
+        self.x_position = x_position
+        self.y_position = y_position
+        self.radius = radius
